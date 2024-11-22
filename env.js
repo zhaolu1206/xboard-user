2140faa8f953dd33138cd24fce0453cf4bd0d241
zhaolu1206 <30510452+zhaolu1206@users.noreply.github.com>
2024-11-22 21:14:38 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://gplus.zl0009.top'
+window.settings = {
+  // 站点名称
+  title: 'G+小机场',
+  // 站点描述
+  description: 'G+小机场',
+  assets_path: '/luke1206',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: 'https://s2.loli.net/2024/11/22/nPvmJTRGckBFtKZ.png',
+}
